aca67f8b18352e1ef3b37a365630ac5a958871bf
Kantapat-Kwansomkid <72593812+Kantapat-Kwansomkid@users.noreply.github.com>
2021-03-28 03:28:43 +0700

    Add files via upload
    คำตอบในข้อที่ 2 ของข้อสอบกลางภาคเรียนรายวิชา MDT112 Workshop (วันเสาร์) ภาคเรียนที่ 2 ปีการศึกษา 2563

---
+++ MDT112_Midterm-Test_LCM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MDT112_Midterm_Test_LCM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, lcm;
+
+            Console.WriteLine("Please input number a : ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please input number b : ");
+            b = int.Parse(Console.ReadLine());
+
+            while(b != 0)
+            {
+                int t = b;
+                b = a % b;
+                a = t;
+            }
+            lcm = (a * b) / a;
+
+            Console.WriteLine("The least common multiple (L.C.M.) is {0}",lcm);
+            Console.ReadLine();
+        }
+    }
+}
